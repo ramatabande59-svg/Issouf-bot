515892c833e066a9c2d7edb6317b58326caaa473
ramatabande59-svg <ramatabande59@gmail.com>
2026-03-02 17:10:11 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,1 @@
+import connectToWhatsapp from './Digix/crew.js'import handleIncomingMessage from './events/messageHandler.js'(async() => {    await connectToWhatsapp(handleIncomingMessage)        console.log('established !')})()
